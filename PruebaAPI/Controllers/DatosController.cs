ee06a901a3efe5659f548730db5a738cfa7a6db1
Jocelyn Berenice Zamora De La Cerda <201G0293@rcarbonifera.tecnm.mx>
2024-04-16 18:32:45 -0600

    Agregar archivos de proyecto.

---
+++ PruebaAPI/Controllers/DatosController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PruebaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DatosController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            string[] datos = new string[] { "Hola Mundo", "Adios" };
+            return Ok(datos);
+        }
+        [HttpGet("numero")]
+        public IActionResult GetNumero()
+        {
+            return Ok(2000);
+        }
+        [HttpGet("{nombre}")]
+        public IActionResult Saludo(string nombre)
+        {
+            return Ok("Hola "+ nombre);
+        }
+        [HttpPost]
+        public IActionResult Post(int numero) 
+        {
+            return Ok(numero*numero); 
+        }
+    }
+}
